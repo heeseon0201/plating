bb1188877234d1bf5c6974cc18b06bea2d597eff
smhrd <smhrd@DESKTOP-E15N0HV>
2021-12-06 18:54:57 +0900

    추가

--- src/main/java/kr/board/domain/User.java
+++ src/main/java/kr/board/domain/User.java
@@ -1,5 +1,7 @@
 package kr.board.domain;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,12 @@ import lombok.NonNull;
 @NoArgsConstructor
 @Data
 public class User {
-   private String userId; // <----> user_id : DB(User table)
-   private String userName;
-   private String userPwd;
-   // setter, getter -> Lombok(롬복)
-   
+	   private String member_id; // <----> user_id : DB(User table)
+	   private String member_pwd;
+	   private String member_nickname;
+	   private String member_email;
+	   private String member_phone;
+	   private Date member_date;
+	   private String member_yesno;
+	   // setter, getter -> Lombok(롬복)
 }
