fb1574c4123cbc17b16e98736753f33a9b5b4d5a
smhrd <smhrd@DESKTOP-G7RUE98>
2021-12-15 15:08:35 +0900

    메이븐 업데이트 문제 해결

--- src/main/java/kr/board/domain/Tbl_ingredient.java
+++ src/main/java/kr/board/domain/Tbl_ingredient.java
@@ -14,16 +14,7 @@ public class Tbl_ingredient {
 	private int ingre_seq;  //재료 순번
 	private String ingre_name;  //재료명
 	private String ingre_content;  //재료 분석
-	private Timestamp reg_date; //등록일자
+	private Timestamp ingre_reg_date; //등록일자
 	private String member_id; //등록자 아이디
-	private String ingre_pic1; //재료이미지1
-	private String ingre_pic2; //재료이미지2
-	private String ingre_pic3; //재료이미지3
-	private String ingre_pic4; //재료이미지4
-	private String ingre_pic5; //재료이미지5
-	private String ingre_pic6; //재료이미지6
-	private String ingre_pic7; //재료이미지7
-	private String ingre_pic8; //재료이미지8
-	private String ingre_pic9; //재료이미지9
-	private String ingre_pic10; //재료이미지10
+	private String ingre_pic; //재료 이미지
 }
